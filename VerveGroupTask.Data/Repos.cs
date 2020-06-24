2bdd134d8837d80758fcb32b17d7d627093b893e
Jason <t7047098@live.tees.ac.uk>
2020-06-24 12:46:20 +0100

    Created Data Classes

---
+++ VerveGroupTask.Data/Repos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VerveGroupTask.Models
+{
+    public class Repos
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Fullname { get; set; }
+        public string Description { get; set; }
+        public int StargazersCount { get; set; }
+        public string Url { get; set; }
+        public int UserId { get; set; }
+        public User Owner { get; set; }
+    }
+}
---
+++ VerveGroupTask.Data/Stargazers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VerveGroupTask.Models
+{
+    public class Stargazers
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public int RepoID { get; set; }
+        public Repos Repo { get; set; }
+    }
+}
