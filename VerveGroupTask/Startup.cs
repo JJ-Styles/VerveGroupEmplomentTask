65a0c6c92ea71eaac90a3b92ba3d7f20b9b4ef79
Jason <t7047098@live.tees.ac.uk>
2020-06-24 13:40:31 +0100

    Updated solution, added packages

--- VerveGroupTask/Startup.cs
+++ VerveGroupTask/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,21 +9,31 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Polly;
 
 namespace VerveGroupTask
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration,
+                        IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment _env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient("RetryAndBreak")
+                    .AddTransientHttpErrorPolicy(p =>
+                        p.OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                         .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))))
+                    .AddTransientHttpErrorPolicy(prop =>
+                        prop.CircuitBreakerAsync(5, TimeSpan.FromSeconds(30)));
             services.AddControllersWithViews();
         }
 
