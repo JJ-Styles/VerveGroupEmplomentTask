64a0c7255928b474daf0a639a902c25dfda53900
Jason <t7047098@live.tees.ac.uk>
2020-06-24 14:01:07 +0100

    Added datd definitions to DTO's

--- VerveGroupTask/Services/RepoDTO.cs
+++ VerveGroupTask/Services/RepoDTO.cs
@@ -2,5 +2,13 @@
 {
     public class RepoDTO
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Fullname { get; set; }
+        public string Description { get; set; }
+        public int StargazersCount { get; set; }
+        public string Url { get; set; }
+        public int UserId { get; set; }
+        public UserDTO Owner { get; set; }
     }
 }
--- VerveGroupTask/Services/StargazerDTo.cs
+++ VerveGroupTask/Services/StargazerDTo.cs
@@ -2,5 +2,9 @@
 {
     public class StargazerDTo
     {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public int RepoID { get; set; }
+        public RepoDTO Repo { get; set; }
     }
 }
